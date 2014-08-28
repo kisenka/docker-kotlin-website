83e1aa8c9a42f03f8f48a4f312c758f903e89cc1
kisenka <qtuzov@gmail.com>
2014-08-28 14:39:59 +0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM kisenka/centos6-jekyll
+
+RUN npm install -g grunt-cli
+
+ADD scripts/ /
+RUN chmod +x /*.sh
+
+RUN /install-wkhtml2pdf.sh
+ADD stuff/10-wkhtmltopdf.conf /etc/fonts/conf.d/10-wkhtmltopdf.conf
+
+RUN yum install -y java-1.7.0-openjdk.x86_64
+RUN cd / && wget https://github.com/JetBrains/kotlin-grammar-generator/releases/download/v0.1/kotlin-grammar-generator.tar.gz
+RUN cd / && tar -xzf ./kotlin-grammar-generator.tar.gz && rm ./kotlin-grammar-generator.tar.gz
+
+CMD rake preview
---
+++ scripts/install-wkhtml2pdf.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# WKHTML2PDF
+wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-centos6-amd64.rpm
+yum localinstall -y wkhtmltox-0.12.1_linux-centos6-amd64.rpm
+rm wkhtmltox-0.12.1_linux-centos6-amd64.rpm
+
+# Fonts
+yum install -y \
+    xorg-x11-fonts-Type1 msttcorefonts \
+    liberation-fonts liberation-fonts-common \
+    liberation-sans-fonts liberation-mono-fonts \
+    liberation-serif-fonts
+
+#wget http://www.my-guides.net/en/images/stories/fedora12/msttcore-fonts-2.0-3.noarch.rpm
+#rpm -Uvh msttcore-fonts-2.0-3.noarch.rpm
